54a39ee69601ddde138f68a36390bc9e2dd8f460
Blaine R Love <blainelove94@gmail.com>
2021-01-03 20:59:26 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,11 @@
 phrase = "Hello there, and how are you?"
 
 def reverse_each_word(phrase)
-  reversed_array = []
+
   new_array = phrase.split
-  new_array.collect {|element| element.reverse}
-    
+  reversed_array = new_array.collect {|element| element.reverse}
+
+
   end
   return reversed_array.join(" ")
 end
