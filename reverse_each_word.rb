79938f03d4b69c8b4c857fbe5e2542d583e48cdf
Blaine R Love <blainelove94@gmail.com>
2021-01-03 20:19:34 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 
-reverse_each_word(reverse_each_word)
-def reverse_each_word
+
+def reverse_each_word("Hello there, and how are you?")
   reverse_each_word.each do |element|
     element.reverse
   end
