48f71f9172116049949a0b6deec183711c39dad1
Blaine R Love <blainelove94@gmail.com>
2021-01-03 20:20:44 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 phrase = "Hello there, and how are you?"
 
-def reverse_each_word("Hello there, and how are you?")
+def reverse_each_word(phrase)
   reverse_each_word.each do |element|
     element.reverse
   end
