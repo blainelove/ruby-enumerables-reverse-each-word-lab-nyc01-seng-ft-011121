4e4ee115d508697bd4c72ebadbe29a95a3130a4e
Blaine R Love <blainelove94@gmail.com>
2021-01-03 20:37:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,8 @@ phrase = "Hello there, and how are you?"
 def reverse_each_word(phrase)
   string = ""
   new_array = []
-  new_array << phrase
+  new_array =phrase.split
+  
   new_array.each do |element|
     string << element.reverse
 
